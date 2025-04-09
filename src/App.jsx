a31be8a434e8c29eebf5d003b7e12f8e5b937b62
Alberto Pimentel <albertopimentel04@gmail.com>
2025-04-09 11:16:44 -0600

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+         <h1>Pokedex - Alberto</h1> 
+         <h2>Welcome to the Pokedex</h2>
+         <p>Here you can find information about your favorite Pokemon.</p>
+
+         <div>
+          <p>Developed by Alberto Pimentel</p>
+          <p>2025</p>
+          <p>All rights reserved</p>
+         </div>
+    </>
+  )
+}
+
+export default App
