bc3727046f592c03e506fbf9f7941ca3173ce58f
Alberto Pimentel <albertopimentel04@gmail.com>
2025-04-10 09:54:48 -0600

    Se agrego la etiqueta img

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ function App() {
   
   return (
     <>
-         <h1>Pokedex - Alberto</h1> 
+         <h1>Pokedex - Alberto Pimentel</h1> 
          <h2>Welcome to the Pokedex</h2>
          <p>Here you can find information about your favorite Pokemon.</p>
-
+       <img src="" alt="" />
          <div>
           <p>Developed by Alberto Pimentel</p>
           <p>2025</p>
